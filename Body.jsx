8cf0a7388bf68604bebc792ef37629bc31257c97
HyoJeong <hyozeong1230@gmail.com>
2023-07-12 15:39:02 +0900

    feat : 마우스로 날짜 선택 및 드래그

--- Body.jsx
+++ Body.jsx
@@ -1,18 +1,19 @@
-const React = require('react');
-const {useState, useEffect} = require('react');
-require('./css/calendar.css');
-
+const React = require("react");
+const { useState, useEffect, useRef } = require("react");
+require("./css/calendar.css");
 
 const Body = ({ currentDate }) => {
   const [selectedDays, setSelectedDays] = useState([]);
-
-  console.log(selectedDays);
+  const [isDragging, setIsDragging] = useState(false); //드래그 여부
+  const [startDay, setStartDay] = useState(null); //드래그 시작 날짜 저장
+  const dragRef = useRef(null); //드래그할 요소 참조 저장
 
   useEffect(() => {
     setSelectedDays([]);
   }, [currentDate]);
 
-  const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+  //요일 표시
+  const weeks = ["일", "월", "화", "수", "목", "금", "토"];
   const displayWeeks = weeks.map((week) => <span key={week}>{week}</span>);
 
   const currentYear = currentDate.getFullYear();
@@ -22,35 +23,119 @@ const Body = ({ currentDate }) => {
   const firstDayOfMonth = new Date(currentYear, currentMonth - 1, 1).getDay();
   const daysOfPrevMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
 
-  const prevDays = Array.from({ length: firstDayOfMonth - 1 }, (_, i) => daysOfPrevMonth - i);
+  const prevDays = Array.from(
+    { length: firstDayOfMonth - 1 },
+    (_, i) => daysOfPrevMonth - i
+  );
   const currentDays = Array.from({ length: daysInMonth }, (_, i) => i + 1);
   const remainingCells = 35 - (prevDays.length + currentDays.length);
   const nextDays = Array.from({ length: remainingCells }, (_, i) => i + 1);
 
-  // 날짜를 클릭 했을 때
-  const handleDayClick = (day) => {
-    // 배열selectedDays에서 선택 날짜 유뮤 확인을 위해 인덱스 값 받아오기
-    const selectedIndex = selectedDays.indexOf(day);
-
-    //선택 날짜가 있을 때 (인덱스 값 : 0 이상)
-    if (selectedIndex > -1) {
-      const updatedSelectedDays = [...selectedDays]; //새 배열 생성
-      updatedSelectedDays.splice(selectedIndex, 1); // 선택 날짜 빼고 유지
-      setSelectedDays(updatedSelectedDays); //selectedDays 업데이트
-
-      //선택 날짜가 없을 때
-    } else { 
-      setSelectedDays([...selectedDays, day]); // 새 배열 생성해 선택 날짜 추가하고 selectedDays 업데이트
+  useEffect(() => {
+    setSelectedDays([]);
+    const savedDates = localStorage.getItem("selectedDates");
+    if (savedDates) {
+      const parsedDates = JSON.parse(savedDates);
+      console.log("Selected Dates:", parsedDates);
+      setSelectedDays(parsedDates.map((dateString) => new Date(dateString))); // Date 객체로 변환
+      // 저장된 날짜를 화면에 표시하는 로직 또는 필요한 곳에서 사용
     }
+  }, [currentDate]);
+
+  // 날짜를 클릭 했을 때
+  const handleDayClick = (day, monthOffset) => {
+    const selectedDate = new Date(
+      currentYear,
+      currentMonth - 1 + monthOffset,
+      day
+    );
+    const selectedIndex = selectedDays.findIndex(
+      (date) => date.toISOString() === selectedDate.toISOString()
+    ); // Date 객체 비교
+    let updatedSelectedDays = [];
+    if (selectedIndex > -1) {
+      updatedSelectedDays = selectedDays.filter(
+        (date) => date.toISOString() !== selectedDate.toISOString()
+      ); // Date 객체 비교
+    } else {
+      updatedSelectedDays = [...selectedDays, selectedDate];
+    }
+    setSelectedDays(updatedSelectedDays);
+
+    const selectedDates = updatedSelectedDays.map((date) => {
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const day = date.getDate().toString().padStart(2, "0");
+      return `${year}년 ${month}월 ${day}일`;
+    });
+
+    localStorage.setItem("selectedDates", JSON.stringify(selectedDates));
+  };
+
+  //날짜 드래그 시작 했을 때
+  const handleDragStart = (day) => {
+    console.log("드래그 시작" + day);
+    setIsDragging(!isDragging);
+    setStartDay(day);
+  };
+
+  //드래그 중
+  const handleDrag = (day) => {
+    console.log("드래그" + day);
+    if (!isDragging) return;
+
+    const startDate = startDay < day ? startDay : day;
+    const endDate = startDay > day ? startDay : day;
+
+    const daysInRange = [];
+    const start = new Date(currentYear, currentMonth - 1, startDate);
+    const end = new Date(currentYear, currentMonth - 1, endDate);
+
+    for (
+      let date = new Date(start);
+      date <= end;
+      date.setDate(date.getDate() + 1)
+    ) {
+      daysInRange.push(new Date(date));
+    }
+
+    setSelectedDays(daysInRange);
+
+    const selectedDates = daysInRange.map((date) => {
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const day = date.getDate().toString().padStart(2, "0");
+      return `${year}년 ${month}월 ${day}일`;
+    });
+
+    localStorage.setItem("selectedDates", JSON.stringify(selectedDates));
+  };
+
+  //드래그 끝
+  const handleDragStop = () => {
+    console.log("드래그 끝");
+    setIsDragging(false);
+    setStartDay(null);
   };
 
   //지난 달 날짜 표시
   const renderPrevDays = () => {
     return prevDays.map((day, index) => (
       <div
-        className={`day notCurrentDays ${selectedDays.includes(day) ? 'selected' : ''}`}
+        className={`day notCurrentDays ${
+          selectedDays.some(
+            (date) =>
+              date.getDate() === day && date.getMonth() === currentMonth - 2
+          )
+            ? "selected"
+            : ""
+        }`}
         key={`prev-${index}`}
-        onClick={() => handleDayClick(day)}
+        onClick={() => handleDayClick(day, -1)} // monthOffset -1로 설정
+        onMouseDown={() => handleDragStart(day)}
+        onMouseUp={handleDragStop}
+        //onMouseLeave={handleDragStop}
+        onMouseOver={() => handleDrag(day)}
       >
         {day}
       </div>
@@ -61,9 +146,21 @@ const Body = ({ currentDate }) => {
   const renderCurrentDays = () => {
     return currentDays.map((day, index) => (
       <div
-        className={`day ${selectedDays.includes(day) ? 'selected' : ''}`}
+        className={`day ${
+          selectedDays.some(
+            (date) =>
+              date.getDate() === day && date.getMonth() === currentMonth - 1
+          )
+            ? "selected"
+            : ""
+        }`}
         key={`current-${index}`}
-        onClick={() => handleDayClick(day)}
+        onClick={() => handleDayClick(day, 0)}
+        onMouseDown={() => handleDragStart(day)}
+        onMouseUp={handleDragStop}
+        //onMouseLeave={handleDragStop}
+        onMouseOver={() => handleDrag(day)}
+        ref={dragRef}
       >
         {day}
       </div>
@@ -74,9 +171,19 @@ const Body = ({ currentDate }) => {
   const renderNextDays = () => {
     return nextDays.map((day, index) => (
       <div
-        className={`day notCurrentDays ${selectedDays.includes(day) ? 'selected' : ''}`}
+        className={`day notCurrentDays ${
+          selectedDays.some(
+            (date) => date.getDate() === day && date.getMonth() === currentMonth
+          )
+            ? "selected"
+            : ""
+        }`}
         key={`next-${index}`}
-        onClick={() => handleDayClick(day)}
+        onClick={() => handleDayClick(day, +1)} // monthOffset +1로 설정
+        onMouseDown={() => handleDragStart(day)}
+        onMouseUp={handleDragStop}
+        //onMouseLeave={handleDragStop}
+        onMouseOver={() => handleDrag(day)}
       >
         {day}
       </div>
