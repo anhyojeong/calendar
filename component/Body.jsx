a550c4955987497061e96a36a3888a941dcfef27
HyoJeong <hyozeong1230@gmail.com>
2023-08-07 13:55:34 +0900

    lazy loading

--- component/Body.jsx
+++ component/Body.jsx
@@ -1,8 +1,8 @@
 const React = require("react");
 const { useState, useEffect, useRef , lazy, Suspense } = require("react");
 const Modal = require("./Modal");
-//const Modal = lazy(() => import("./Modal"));
-const { LoadStorage } = require("../hooks/LoadStorage");
+const Modal = lazy(() => import("./Modal"));
+//const { LoadStorage } = require("../hooks/LoadStorage");
 require("../css/calendar.css");
 
 const Body = ({ currentDate }) => {
@@ -204,13 +204,13 @@ const Body = ({ currentDate }) => {
         {renderDays(nextDays, 1)}
       </div>
       {isModalOpen && (
-         //<Suspense fallback={<div>Loading...</div>}>
+         <Suspense fallback={<div>Loading...</div>}>
         <Modal
           closeModal={handleCloseModal}
           selectedDate={selectedDays}
           setNullSelectedDays={() => setSelectedDays([])}
         />
-       //</Suspense>
+       </Suspense>
       )}
     </div>
   );
